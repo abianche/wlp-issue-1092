48335d9afe53cffd3d47693b76342c7081cddc3c
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2024-11-17 22:53:23 +0100

    1092

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
+import path from "path";
+import * as R from "ramda";
+import type { Configuration as WebpackConfig } from "webpack";
+import LicensePlugin from "webpack-license-plugin";
+
+const packageJson = require(path.resolve(__dirname, "package.json"));
+const dependenciesPaths = R.map(
+  (name) => R.concat("node_modules/", name),
+  R.keys(packageJson.dependencies) as string[]
+);
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config: WebpackConfig) => {
+    config.plugins = config.plugins || [];
+    config.plugins.push(
+      new LicensePlugin()
+      // { includePackages: () => dependenciesPaths }
+    );
+
+    return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
